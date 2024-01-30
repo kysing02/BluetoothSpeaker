d37c82a2f63661505fe5c851ae2c77b389d2eabf
kysing02 <yongsing1110@gmail.com>
2024-01-30 17:25:47 +0900

    Bluetooth仕上げ

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def main():
             print('Error: Media Player not found. Retrying...')
         else:
             bluetooth_available = True
-            print(transport_prop_iface.GetAll('org.freedesktop.DBus.Properties'));
+            print(transport_prop_iface.GetAll('org.bluez'));
         if not transport_prop_iface:
             print('Error: DBus.Properties iface not found. Retrying...')
             time.sleep(5)
